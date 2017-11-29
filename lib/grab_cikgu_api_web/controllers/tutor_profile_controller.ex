f475d0d8bd236437566c9ca0df93658a636198a6
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-11-29 15:36:12 +0800

    Show tutor profile view

--- lib/grab_cikgu_api_web/controllers/tutor_profile_controller.ex
+++ lib/grab_cikgu_api_web/controllers/tutor_profile_controller.ex
@@ -2,7 +2,8 @@ defmodule GrabCikguApiWeb.TutorProfileController do
   use GrabCikguApiWeb, :controller
 
   def show(conn, _params) do
-    render(conn, "show.json", %{})
+    tutor = conn.assigns.current_user
+    render(conn, "show.json", %{tutor: tutor})
   end
 
 end
