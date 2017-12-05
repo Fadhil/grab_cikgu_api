d340eaca22b14f4c03f331b956fe64e891d31825
Fadhil Luqman <fadhil.luqman@gmail.com>
2017-12-05 13:31:19 +0800

    add server: true to config prod

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,8 @@ config :grab_cikgu_api, GrabCikguApiWeb.Endpoint,
 	http: [port: 8888],
   load_from_system_env: true,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+	server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
